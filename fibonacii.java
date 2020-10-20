250d46871087c48815d30b852d0b162443a9f82d
nrusingha.acharya <nrusingha.acharya@gmail.com>
2020-10-20 12:59:26 +0000

    fibonacii.java  updated

--- fibonacii.java
+++ fibonacii.java
@@ -1,22 +1,21 @@
 class Fibonacci { 
 	public static void main(String[] args) { 
 		int n = 10, t1 = 0, t2 = 1;
-	       	System.out.print("First " +  n + " terms: \n ");		        
+//	       	System.out.print("First " +  n + " terms: \n ");		        
 		System.out.println(" t1 " + " t2 " + " sum " + "\n" );
 	       	System.out.println(" --------------------------- " +  "\n" );
-
-        	for (int i = 1; i <= n; ++i) { 
-			System.out.println( t1 + " " ); 
+	      	for (int i = 1; i <= n; ++i) { 
+			
 			int sum = t1 + t2; 
 			t1 = t2; 
 			t2 = sum; 
-			if (t1 == 0)
-		 		System.out.println( " " + t2 + " " + sum + "\n " ); 
-			else 
-				System.out.println( t1 + " " + t2 + " " + 
+	//		if (t1 == 0)
+		 		System.out.println( t1 + " " + t2 + " " + sum + "\n " ); 
+	//		else 
+	//			System.out.println( t1 + " " + t2 + " " + 
 
 						
-			sum + "\n " );
+//			sum + "\n " );
 
 		
         }
