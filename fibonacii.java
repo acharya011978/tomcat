6909f36069da7529013758e75dfc572bfcf717c6
nrusingha.acharya <nrusingha.acharya@gmail.com>
2020-10-20 11:59:06 +0000

    adding fibonacie from newgit

---
+++ fibonacii.java
@@ -0,0 +1,25 @@
+class Fibonacci { 
+	public static void main(String[] args) { 
+		int n = 10, t1 = 0, t2 = 1;
+	       	System.out.print("First " +  n + " terms: \n ");		        
+		System.out.println(" t1 " + " t2 " + " sum " + "\n" );
+	       	System.out.println(" --------------------------- " +  "\n" );
+
+        	for (int i = 1; i <= n; ++i) { 
+			System.out.println( t1 + " " ); 
+			int sum = t1 + t2; 
+			t1 = t2; 
+			t2 = sum; 
+			if (t1 == 0)
+		 		System.out.println( " " + t2 + " " + sum + "\n " ); 
+			else 
+				System.out.println( t1 + " " + t2 + " " + 
+
+						
+			sum + "\n " );
+
+		
+        }
+    }
+}
+
